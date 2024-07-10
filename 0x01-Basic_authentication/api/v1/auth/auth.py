6798e5c0d6fcfc3b86a5d7d0f68fd0bbaa1909af
Badr_Annabi <badrannabi@gmail.com>
2024-07-10 11:21:14 +0100

    TASK 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env pyhton3
+"""Create a class to manage the Api auth"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Manages the API Authentication"""
+    def __init__(self):
+        """"""
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method that requires auth"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Public method for authorization header"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
+        return None
