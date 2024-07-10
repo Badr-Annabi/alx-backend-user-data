50763df92ab441513bb28f380605115f6355cffd
Badr_Annabi <badrannabi@gmail.com>
2024-07-10 12:23:35 +0100

    TASK 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,11 +8,16 @@ from typing import Tuple
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'default')
+if auth_type == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -48,6 +53,21 @@ def forbidden(error: Exception) -> Tuple[jsonify, int]:
         status code.
     """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def handle_request():
+    """Handle req by checking for auth"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
