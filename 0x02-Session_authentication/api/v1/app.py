846286585a15be01a1b94d1ef20fcaa1ee958567
Badr_Annabi <badrannabi@gmail.com>
2024-07-13 00:28:49 +0100

    TASK 10

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDbAuth
 
 
 app = Flask(__name__)
@@ -24,6 +25,8 @@ if auth_type == "session_auth":
     auth = SessionAuth()
 elif auth_type == "session_exp_auth":
     auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    auth = SessionDbAuth()
 elif auth_type == 'basic_auth':
     auth = BasicAuth()
 else:
