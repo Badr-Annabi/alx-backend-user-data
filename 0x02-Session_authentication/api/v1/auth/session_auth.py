08eae44a4be7478efb41aa527accaa45eb9b7c4f
Badr_Annabi <badrannabi@gmail.com>
2024-07-12 01:46:03 +0100

    TASK 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,7 @@ from .auth import Auth
 class SessionAuth(Auth):
     """Session auth class that inherits from Auth class"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """This method creates a session ID for user_id"""
         if user_id is None:
