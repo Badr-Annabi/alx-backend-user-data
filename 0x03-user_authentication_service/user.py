2f652ddcdd6993bac87743e2e2f94171fd857c19
Badr_Annabi <badrannabi@gmail.com>
2024-07-17 18:48:13 +0100

    test

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,8 +5,8 @@ for a database table named users
 """
 
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -14,7 +14,8 @@ Base = declarative_base()
 class User(Base):
     """SQLAlchemy declarative User class(model)"""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
