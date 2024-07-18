e572ae32850256e10cbba0bc6f551680e053f06a
Badr_Annabi <badrannabi@gmail.com>
2024-07-18 02:46:54 +0100

    TASK 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> bytes:
     """takes a password and returns a hashed password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
