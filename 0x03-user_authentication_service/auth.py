66b64bab483d69ec91210f2c86695d1904e83859
Badr_Annabi <badrannabi@gmail.com>
2024-07-18 02:44:30 +0100

    TASK 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/bin/usr/env python3
+"""creae a method for encrypting passwords"""
+import bcrypt
+
+
+def _hash_password(password):
+    """takes a password and returns a hashed password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
