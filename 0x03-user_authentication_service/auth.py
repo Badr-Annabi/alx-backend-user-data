b2abb74724919266e920cac911f0742d816ab782
Badr_Annabi <badrannabi@gmail.com>
2024-07-18 15:56:44 +0100

    TASK 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
-"""Module for authentication service"""
-from bcrypt import hashpw, gensalt
+""" auth script defines _hash_password method for now """
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
-    """takes a password and returns a hashed password"""
-    encoded_bytes = password.encode('utf-8')
+    """ hashes a password using bcrypt.hashpw"""
+    bytes = password.encode("utf-8")
+
     salt = gensalt()
-    hash_password = hashpw(encoded_bytes, salt)
-    return hash_password
+
+    hash = hashpw(bytes, salt)
+    return(hash)
+
+
+def _generate_uuid() -> str:
+    """ returns a string representation of a new UUID"""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -22,7 +30,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """this method registers a new user"""
+        """ save a user, if does'nt exist, with hashed password"""
         hashed_pwd = _hash_password(password)
         try:
             usr = self._db.find_user_by(email=email)
