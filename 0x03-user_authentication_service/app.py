9f7f048c7d6dcbed61862f9c99478cdd5be6c5f2
Badr_Annabi <badrannabi@gmail.com>
2024-07-19 01:13:01 +0100

    TASK 11 add POST method for /session route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from auth import Auth
-from flask import Flask, abort, request, jsonify
+from flask import Flask, abort, request, jsonify, make_response
 
 Auth = Auth()
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
+def index():
     """
     GET /
     Return: payload containing a welcome message
@@ -17,7 +17,7 @@ def index() -> str:
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
+def users():
     """this function is used to register a new user"""
     email, password = request.form.get("email"), request.form.get("password")
     try:
@@ -37,7 +37,8 @@ def login():
     if not Auth.valid_login(email, password):
         abort(401)
     session_id = Auth.create_session(email)
-    res = jsonify({"email": email, "message": "logged in"})
+    res = make_response(
+        jsonify({"email": email, "message": "logged in"}))
     res.set_cookie("session_id", session_id)
     return res
 
